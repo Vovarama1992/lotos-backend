6889566f3f85b31099659a110b7771769d8cc4c4
Maxim <mladoshin@mail.ru>
2024-07-27 19:26:17 +0300

    fix: fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@ git reset --hard
 git pull
 npm run build
 pm2 delete casino_api
-if [[ $env = "staging" ]] then
+if [[ $env = "staging" ]]; then
     export NODE_ENV="staging"
-elif [[ $env = "prod" ]] then
+elif [[ $env = "prod" ]]; then
     export NODE_ENV="prod"
 fi
 pm2 start dist/main.js --name casino_api
