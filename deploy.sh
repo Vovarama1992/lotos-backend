6e4f3d10f008f352f14c7df012c3942e4c56fae3
Maxim <mladoshin@mail.ru>
2024-07-27 19:24:54 +0300

    fix: changed staging ssl

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
+env=$1
 git reset --hard
 git pull
 npm run build
 pm2 delete casino_api
+if [[ $env = "staging" ]] then
+    export NODE_ENV="staging"
+elif [[ $env = "prod" ]] then
+    export NODE_ENV="prod"
+fi
 pm2 start dist/main.js --name casino_api
